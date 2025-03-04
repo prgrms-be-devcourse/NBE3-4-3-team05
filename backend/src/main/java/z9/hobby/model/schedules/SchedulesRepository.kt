e0d88db62afdc0e993c867b740149df0d9d3b585
davinyakma <poohcdv3652@naver.com>
2025-03-04 17:47:33 +0900

    Refactor: SchedulesRepository 코틀린 변환

--- backend/src/main/java/z9/hobby/model/schedules/SchedulesRepository.kt
+++ backend/src/main/java/z9/hobby/model/schedules/SchedulesRepository.kt
@@ -1,24 +1,27 @@
-package z9.hobby.model.schedules;
+package z9.hobby.model.schedules
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import java.util.*
 
-import java.util.List;
-import java.util.Optional;
-
-public interface SchedulesRepository extends JpaRepository<SchedulesEntity, Long> {
+interface SchedulesRepository : JpaRepository<SchedulesEntity, Long> {
     // 클래스에 속한 모든 일정 조회
     @Query("SELECT s FROM SchedulesEntity s JOIN FETCH s.classes WHERE s.classes.id = :classId")
-    List<SchedulesEntity> findSchedulesByClassesId(@Param("classId") Long classId);
+    fun findSchedulesByClassesId(@Param("classId") classId: Long): List<SchedulesEntity>
 
     // 특정 클래스의 특정 일정 조회 (modify, delete, getScheduleDetail에서 사용)
     @Query("SELECT s FROM SchedulesEntity s JOIN FETCH s.classes c WHERE s.id = :scheduleId AND c.id = :classId")
-    Optional<SchedulesEntity> findScheduleByIdAndClassesId(@Param("scheduleId") Long scheduleId, @Param("classId") Long classId);
+    fun findScheduleByIdAndClassesId(
+        @Param("scheduleId") scheduleId: Long,
+        @Param("classId") classId: Long
+    ): Optional<SchedulesEntity>
 
-    @Query("SELECT s FROM SchedulesEntity s " +
-            "JOIN FETCH s.checkins sc " +
-            "WHERE sc.userId = :userId AND sc.checkIn = true " +
-            "ORDER BY s.meetingTime DESC")
-    List<SchedulesEntity> findUserSchedulesInfoByUserId(@Param("userId") Long userId);
+    @Query(
+        ("SELECT s FROM SchedulesEntity s " +
+                "JOIN FETCH s.checkins sc " +
+                "WHERE sc.userId = :userId AND sc.checkIn = true " +
+                "ORDER BY s.meetingTime DESC")
+    )
+    fun findUserSchedulesInfoByUserId(@Param("userId") userId: Long): List<SchedulesEntity>
 }
