225297dc8733e2b059486a572cf3065d5b757855
davinyakma <poohcdv3652@naver.com>
2025-03-05 12:33:41 +0900

    Refactor: SchedulesEntity 코틀린 변환

--- backend/src/main/java/z9/hobby/model/schedules/SchedulesEntity.kt
+++ backend/src/main/java/z9/hobby/model/schedules/SchedulesEntity.kt
@@ -1,45 +1,66 @@
-package z9.hobby.model.schedules;
+package z9.hobby.model.schedules
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import z9.hobby.domain.classes.entity.ClassEntity;
-import z9.hobby.model.BaseEntity;
-import z9.hobby.model.checkIn.CheckInEntity;
-
-import java.util.ArrayList;
-import java.util.List;
+import jakarta.persistence.*
+import z9.hobby.domain.classes.entity.ClassEntity
+import z9.hobby.model.BaseEntity
+import z9.hobby.model.checkIn.CheckInEntity
 
 @Entity
 @Table(name = "schedules")
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class SchedulesEntity extends BaseEntity {
+class SchedulesEntity(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "schedules_id", nullable = false)
-    private Long id;
+    private val id: Long? = null,
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "class_id")
-    private ClassEntity classes;
+    private val classes: ClassEntity? = null,
 
     @Column(name = "meeting_time", nullable = false)
-    private String meetingTime;
+    private var meetingTime: String? = null,
 
     @Column(name = "meeting_title", nullable = false)
-    private String meetingTitle;
+    private var meetingTitle: String? = null,
 
-    @Builder.Default
-    @OneToMany(mappedBy = "schedules", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<CheckInEntity> checkins = new ArrayList<>();
+    @OneToMany(mappedBy = "schedules", cascade = [CascadeType.ALL], orphanRemoval = true)
+    private var checkins: MutableList<CheckInEntity> = mutableListOf()
+) : BaseEntity() {
+    // Getters
+    fun getId(): Long? = id
+    fun getClasses(): ClassEntity = classes ?: throw IllegalStateException("Classes should not be null")
+    fun getMeetingTime(): String? = meetingTime
+    fun getMeetingTitle(): String? = meetingTitle
+    fun getCheckins(): List<CheckInEntity> = checkins
 
-    public void updateSchedule(String meetingTime, String meetingTitle) {
-        this.meetingTime = meetingTime;
-        this.meetingTitle = meetingTitle;
+    fun updateSchedule(meetingTime: String?, meetingTitle: String?) {
+        this.meetingTime = meetingTime
+        this.meetingTitle = meetingTitle
+    }
+    // Builder pattern implementation in Kotlin
+    companion object {
+        fun builder() = Builder()
+    }
+
+    class Builder {
+        private var id: Long? = null
+        private var classes: ClassEntity? = null
+        private var meetingTime: String? = null
+        private var meetingTitle: String? = null
+        private var checkins: MutableList<CheckInEntity> = mutableListOf()
+
+        fun id(id: Long?) = apply { this.id = id }
+        fun classes(classes: ClassEntity?) = apply { this.classes = classes }
+        fun meetingTime(meetingTime: String?) = apply { this.meetingTime = meetingTime }
+        fun meetingTitle(meetingTitle: String?) = apply { this.meetingTitle = meetingTitle }
+        fun checkins(checkins: List<CheckInEntity>) = apply { this.checkins.addAll(checkins) }
+
+        fun build() = SchedulesEntity(
+            id = id,
+            classes = classes,
+            meetingTime = meetingTime,
+            meetingTitle = meetingTitle,
+            checkins = checkins
+        )
     }
 }
