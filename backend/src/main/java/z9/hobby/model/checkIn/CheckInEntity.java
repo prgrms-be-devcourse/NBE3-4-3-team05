12af532751fd1f3f9eaa5857161544ff5b4426d3
이장호 <jho951@naver.com>
2025-03-05 16:02:57 +0900

    Revert "Build : 테스트 (#7)"
    This reverts commit f7edb48464d997aceaaa7f5c14d1e87a97898133.

--- backend/src/main/java/z9/hobby/model/checkIn/CheckInEntity.java
+++ backend/src/main/java/z9/hobby/model/checkIn/CheckInEntity.java
@@ -19,11 +19,14 @@ public class CheckInEntity extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "sc_id", nullable = false)
     private Long id;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "schedules_id")
     private SchedulesEntity schedules;
+
     @Column(name = "user_id", nullable = false)
     private Long userId;
+
     @Column(name = "check_in", nullable = false)
     private boolean checkIn;
 }
