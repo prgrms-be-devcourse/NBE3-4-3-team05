cceb116c92266a4d9bc57b89d88ef7dee59b47a0
KangBaekGwa <ksu9801@gmail.com>
2025-03-05 10:53:13 +0900

    refactor: Migrate UserFavorite domain to Kotlin

--- backend/src/main/java/z9/hobby/model/userfavorite/UserFavoriteRepository.kt
+++ backend/src/main/java/z9/hobby/model/userfavorite/UserFavoriteRepository.kt
@@ -1,14 +1,13 @@
-package z9.hobby.model.userfavorite;
+package z9.hobby.model.userfavorite
 
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
-public interface UserFavoriteRepository extends JpaRepository<UserFavorite, Long> {
+interface UserFavoriteRepository : JpaRepository<UserFavorite, Long> {
 
-    void deleteByUserId(Long userId);
+    fun deleteByUserId(userId: Long)
 
     @Query("SELECT f.name FROM UserFavorite uf JOIN uf.favorite f WHERE uf.user.id = :userId")
-    List<String> findFavoriteNamesByUserId(@Param("userId") Long userId);
+    fun findFavoriteNamesByUserId(@Param("userId") userId: Long): List<String>
 }
