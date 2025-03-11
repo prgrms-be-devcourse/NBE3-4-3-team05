c5963874e12e6d4472332afa06285abb3df3b48d
KangBaekGwa <ksu9801@gmail.com>
2025-03-11 13:55:46 +0900

    feat : 테이블 생성 Flyway 로 통일

---
+++ backend/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,146 @@
+-- 외래 키 제약 조건 비활성화
+SET
+FOREIGN_KEY_CHECKS = 0;
+
+-- 테이블 삭제 (FK 의존 관계 고려하여 순서대로 삭제)
+DROP TABLE IF EXISTS schedules_checkin;
+DROP TABLE IF EXISTS schedules;
+DROP TABLE IF EXISTS class_user_entity;
+DROP TABLE IF EXISTS class_black_list_entity;
+DROP TABLE IF EXISTS class_entity;
+DROP TABLE IF EXISTS users_favorite;
+DROP TABLE IF EXISTS users_oauth;
+DROP TABLE IF EXISTS favorites;
+DROP TABLE IF EXISTS users;
+
+-- 외래 키 제약 조건 활성화
+SET
+FOREIGN_KEY_CHECKS = 1;
+
+-- 데이터베이스 생성
+CREATE
+DATABASE IF NOT EXISTS hobby;
+USE
+hobby;
+
+-- User Table
+CREATE TABLE `users`
+(
+    `user_id`     bigint      NOT NULL AUTO_INCREMENT,
+    `created_at`  datetime(6) DEFAULT NULL,
+    `modified_at` datetime(6) DEFAULT NULL,
+    `login_id`    varchar(255) DEFAULT NULL,
+    `nickname`    varchar(10) NOT NULL,
+    `password`    varchar(255) DEFAULT NULL,
+    `role`        enum('ROLE_ADMIN','ROLE_USER') NOT NULL,
+    `status`      enum('ACTIVE','DELETE','PENDING') NOT NULL,
+    `type`        enum('NORMAL','OAUTH') NOT NULL,
+    PRIMARY KEY (`user_id`),
+    UNIQUE KEY `unique_nickname` (`nickname`),
+    UNIQUE KEY `unique_login_id` (`login_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Favorite Table
+CREATE TABLE `favorites`
+(
+    `favorite_id`   bigint       NOT NULL AUTO_INCREMENT,
+    `favorite_name` varchar(255) NOT NULL,
+    PRIMARY KEY (`favorite_id`),
+    INDEX           `idx_favorite_name` (`favorite_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- UserOauth Table
+CREATE TABLE `users_oauth`
+(
+    `users_oauth_id` bigint NOT NULL AUTO_INCREMENT,
+    `created_at`     datetime(6) DEFAULT NULL,
+    `modified_at`    datetime(6) DEFAULT NULL,
+    `provider`       enum('GOOGLE','KAKAO','NAVER') DEFAULT NULL,
+    `uid`            varchar(255) DEFAULT NULL,
+    `user_id`        bigint       DEFAULT NULL,
+    PRIMARY KEY (`users_oauth_id`),
+    UNIQUE KEY `unique_uid` (`uid`),
+    KEY              `fk_user_id` (`user_id`),
+    CONSTRAINT `fk_user_oauth_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- User Favorite Table
+CREATE TABLE `users_favorite`
+(
+    `user_favorite_id` bigint NOT NULL AUTO_INCREMENT,
+    `favorite_id`      bigint DEFAULT NULL,
+    `user_id`          bigint DEFAULT NULL,
+    PRIMARY KEY (`user_favorite_id`),
+    KEY                `idx_favorite_id` (`favorite_id`),
+    KEY                `idx_user_id` (`user_id`),
+    CONSTRAINT `fk_users_favorite_favorite_id` FOREIGN KEY (`favorite_id`) REFERENCES `favorites` (`favorite_id`) ON DELETE CASCADE,
+    CONSTRAINT `fk_users_favorite_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Class Table
+CREATE TABLE `class_entity`
+(
+    `class_id`    bigint       NOT NULL AUTO_INCREMENT,
+    `created_at`  datetime(6) DEFAULT NULL,
+    `modified_at` datetime(6) DEFAULT NULL,
+    `description` text         NOT NULL,
+    `favorite`    varchar(255) NOT NULL,
+    `master_id`   bigint       NOT NULL,
+    `name`        varchar(255) NOT NULL,
+    PRIMARY KEY (`class_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Class Black List Table
+CREATE TABLE `class_black_list_entity`
+(
+    `cbl_id`   bigint NOT NULL AUTO_INCREMENT,
+    `user_id`  bigint NOT NULL,
+    `class_id` bigint NOT NULL,
+    PRIMARY KEY (`cbl_id`),
+    KEY        `FK_class_black_list_class_id` (`class_id`),
+    CONSTRAINT `FK_class_black_list_to_class_entity` FOREIGN KEY (`class_id`) REFERENCES `class_entity` (`class_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Class User Table
+CREATE TABLE `class_user_entity`
+(
+    `cu_id`    bigint NOT NULL AUTO_INCREMENT,
+    `user_id`  bigint NOT NULL,
+    `class_id` bigint NOT NULL,
+    PRIMARY KEY (`cu_id`),
+    KEY        `idx_class_id` (`class_id`),
+    CONSTRAINT `fk_class_user_entity_class_id` FOREIGN KEY (`class_id`) REFERENCES `class_entity` (`class_id`) ON DELETE CASCADE,
+    INDEX      `idx_user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Schedule Table
+CREATE TABLE `schedules`
+(
+    `lat` double NOT NULL,
+    `lng` double NOT NULL,
+    `class_id`      bigint DEFAULT NULL,
+    `created_at`    datetime(6) DEFAULT NULL,
+    `modified_at`   datetime(6) DEFAULT NULL,
+    `schedules_id`  bigint       NOT NULL AUTO_INCREMENT,
+    `meeting_place` varchar(255) NOT NULL,
+    `meeting_time`  varchar(255) NOT NULL,
+    `meeting_title` varchar(255) NOT NULL,
+    PRIMARY KEY (`schedules_id`),
+    CONSTRAINT `FK_schedules_class_id` FOREIGN KEY (`class_id`) REFERENCES `class_entity` (`class_id`) ON DELETE CASCADE,
+    KEY             `IDX_schedules_class_id` (`class_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Schedule Checkin Table
+CREATE TABLE `schedules_checkin`
+(
+    `sc_id`        bigint NOT NULL AUTO_INCREMENT,
+    `created_at`   datetime(6) DEFAULT NULL,
+    `modified_at`  datetime(6) DEFAULT NULL,
+    `check_in`     bit(1) NOT NULL,
+    `user_id`      bigint NOT NULL,
+    `schedules_id` bigint DEFAULT NULL,
+    PRIMARY KEY (`sc_id`),
+    CONSTRAINT `FK_schedules_checkin_schedules_id` FOREIGN KEY (`schedules_id`) REFERENCES `schedules` (`schedules_id`) ON DELETE CASCADE,
+    KEY            `IDX_schedules_checkin_schedules_id` (`schedules_id`),
+    INDEX          `idx_user_checkin_user_id_check_in` (`user_id`, `check_in`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
---
+++ backend/src/main/resources/db/seed/R__A_insert_seed_user.sql
@@ -0,0 +1,12 @@
+INSERT INTO users (login_id, password, nickname, type, status, role, created_at, modified_at)
+VALUES
+    ('test1@email.com', '$2a$10$w9hKeWwb7pLRVauv/6DOK.it239jAio9luYL2iSYeylIRHhysciMm', 'test1', 'NORMAL', 'ACTIVE', 'ROLE_USER', CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6)),
+    ('test2@email.com', '$2a$10$w9hKeWwb7pLRVauv/6DOK.it239jAio9luYL2iSYeylIRHhysciMm', 'test2', 'NORMAL', 'ACTIVE', 'ROLE_USER', CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6)),
+    ('test3@email.com', '$2a$10$w9hKeWwb7pLRVauv/6DOK.it239jAio9luYL2iSYeylIRHhysciMm', 'test3', 'NORMAL', 'ACTIVE', 'ROLE_USER', CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6)),
+    ('test4@email.com', '$2a$10$w9hKeWwb7pLRVauv/6DOK.it239jAio9luYL2iSYeylIRHhysciMm', 'test4', 'NORMAL', 'ACTIVE', 'ROLE_USER', CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6)),
+    ('test5@email.com', '$2a$10$w9hKeWwb7pLRVauv/6DOK.it239jAio9luYL2iSYeylIRHhysciMm', 'test5', 'NORMAL', 'ACTIVE', 'ROLE_USER', CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6)),
+    ('test6@email.com', '$2a$10$w9hKeWwb7pLRVauv/6DOK.it239jAio9luYL2iSYeylIRHhysciMm', 'test6', 'NORMAL', 'ACTIVE', 'ROLE_USER', CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6)),
+    ('test7@email.com', '$2a$10$w9hKeWwb7pLRVauv/6DOK.it239jAio9luYL2iSYeylIRHhysciMm', 'test7', 'NORMAL', 'ACTIVE', 'ROLE_USER', CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6)),
+    ('test8@email.com', '$2a$10$w9hKeWwb7pLRVauv/6DOK.it239jAio9luYL2iSYeylIRHhysciMm', 'test8', 'NORMAL', 'ACTIVE', 'ROLE_USER', CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6)),
+    ('test9@email.com', '$2a$10$w9hKeWwb7pLRVauv/6DOK.it239jAio9luYL2iSYeylIRHhysciMm', 'test9', 'NORMAL', 'ACTIVE', 'ROLE_USER', CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6)),
+    ('test10@email.com', '$2a$10$w9hKeWwb7pLRVauv/6DOK.it239jAio9luYL2iSYeylIRHhysciMm', 'test10', 'NORMAL', 'ACTIVE', 'ROLE_USER', CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6));
