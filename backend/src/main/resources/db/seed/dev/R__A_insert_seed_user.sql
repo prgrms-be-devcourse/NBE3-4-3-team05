9dad0fd0183c775c3b8e4ad8df38b8e090b8834c
KangBaekGwa <ksu9801@gmail.com>
2025-03-13 04:21:25 +0900

    refactor : 회원 생성 프로시저 + 반복문 방식으로 변경

--- backend/src/main/resources/db/seed/dev/R__A_insert_seed_user.sql
+++ backend/src/main/resources/db/seed/dev/R__A_insert_seed_user.sql
@@ -1,12 +1,26 @@
-INSERT INTO users (login_id, password, nickname, type, status, role, created_at, modified_at)
-VALUES
-    ('test1@email.com', '$2a$10$w9hKeWwb7pLRVauv/6DOK.it239jAio9luYL2iSYeylIRHhysciMm', 'test1', 'NORMAL', 'ACTIVE', 'ROLE_USER', CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6)),
-    ('test2@email.com', '$2a$10$w9hKeWwb7pLRVauv/6DOK.it239jAio9luYL2iSYeylIRHhysciMm', 'test2', 'NORMAL', 'ACTIVE', 'ROLE_USER', CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6)),
-    ('test3@email.com', '$2a$10$w9hKeWwb7pLRVauv/6DOK.it239jAio9luYL2iSYeylIRHhysciMm', 'test3', 'NORMAL', 'ACTIVE', 'ROLE_USER', CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6)),
-    ('test4@email.com', '$2a$10$w9hKeWwb7pLRVauv/6DOK.it239jAio9luYL2iSYeylIRHhysciMm', 'test4', 'NORMAL', 'ACTIVE', 'ROLE_USER', CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6)),
-    ('test5@email.com', '$2a$10$w9hKeWwb7pLRVauv/6DOK.it239jAio9luYL2iSYeylIRHhysciMm', 'test5', 'NORMAL', 'ACTIVE', 'ROLE_USER', CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6)),
-    ('test6@email.com', '$2a$10$w9hKeWwb7pLRVauv/6DOK.it239jAio9luYL2iSYeylIRHhysciMm', 'test6', 'NORMAL', 'ACTIVE', 'ROLE_USER', CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6)),
-    ('test7@email.com', '$2a$10$w9hKeWwb7pLRVauv/6DOK.it239jAio9luYL2iSYeylIRHhysciMm', 'test7', 'NORMAL', 'ACTIVE', 'ROLE_USER', CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6)),
-    ('test8@email.com', '$2a$10$w9hKeWwb7pLRVauv/6DOK.it239jAio9luYL2iSYeylIRHhysciMm', 'test8', 'NORMAL', 'ACTIVE', 'ROLE_USER', CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6)),
-    ('test9@email.com', '$2a$10$w9hKeWwb7pLRVauv/6DOK.it239jAio9luYL2iSYeylIRHhysciMm', 'test9', 'NORMAL', 'ACTIVE', 'ROLE_USER', CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6)),
-    ('test10@email.com', '$2a$10$w9hKeWwb7pLRVauv/6DOK.it239jAio9luYL2iSYeylIRHhysciMm', 'test10', 'NORMAL', 'ACTIVE', 'ROLE_USER', CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6));
+DELIMITER $$
+CREATE PROCEDURE InsertBatchData()
+BEGIN
+    DECLARE counter INT DEFAULT 1;
+    WHILE counter <= 10 DO
+            INSERT INTO users (login_id, password, nickname, type, status, role, created_at, modified_at)
+            VALUES
+                (CONCAT('test', counter, '@email.com'), '$2a$10$w9hKeWwb7pLRVauv/6DOK.it239jAio9luYL2iSYeylIRHhysciMm', CONCAT('test', counter), 'NORMAL', 'ACTIVE', 'ROLE_USER', CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6)),
+                (CONCAT('test', counter+1, '@email.com'), '$2a$10$w9hKeWwb7pLRVauv/6DOK.it239jAio9luYL2iSYeylIRHhysciMm', CONCAT('test', counter+1), 'NORMAL', 'ACTIVE', 'ROLE_USER', CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6)),
+                (CONCAT('test', counter+2, '@email.com'), '$2a$10$w9hKeWwb7pLRVauv/6DOK.it239jAio9luYL2iSYeylIRHhysciMm', CONCAT('test', counter+2), 'NORMAL', 'ACTIVE', 'ROLE_USER', CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6)),
+                (CONCAT('test', counter+3, '@email.com'), '$2a$10$w9hKeWwb7pLRVauv/6DOK.it239jAio9luYL2iSYeylIRHhysciMm', CONCAT('test', counter+3), 'NORMAL', 'ACTIVE', 'ROLE_USER', CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6)),
+                (CONCAT('test', counter+4, '@email.com'), '$2a$10$w9hKeWwb7pLRVauv/6DOK.it239jAio9luYL2iSYeylIRHhysciMm', CONCAT('test', counter+4), 'NORMAL', 'ACTIVE', 'ROLE_USER', CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6)),
+                (CONCAT('test', counter+5, '@email.com'), '$2a$10$w9hKeWwb7pLRVauv/6DOK.it239jAio9luYL2iSYeylIRHhysciMm', CONCAT('test', counter+5), 'NORMAL', 'ACTIVE', 'ROLE_USER', CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6)),
+                (CONCAT('test', counter+6, '@email.com'), '$2a$10$w9hKeWwb7pLRVauv/6DOK.it239jAio9luYL2iSYeylIRHhysciMm', CONCAT('test', counter+6), 'NORMAL', 'ACTIVE', 'ROLE_USER', CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6)),
+                (CONCAT('test', counter+7, '@email.com'), '$2a$10$w9hKeWwb7pLRVauv/6DOK.it239jAio9luYL2iSYeylIRHhysciMm', CONCAT('test', counter+7), 'NORMAL', 'ACTIVE', 'ROLE_USER', CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6)),
+                (CONCAT('test', counter+8, '@email.com'), '$2a$10$w9hKeWwb7pLRVauv/6DOK.it239jAio9luYL2iSYeylIRHhysciMm', CONCAT('test', counter+8), 'NORMAL', 'ACTIVE', 'ROLE_USER', CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6)),
+                (CONCAT('test', counter+9, '@email.com'), '$2a$10$w9hKeWwb7pLRVauv/6DOK.it239jAio9luYL2iSYeylIRHhysciMm', CONCAT('test', counter+9), 'NORMAL', 'ACTIVE', 'ROLE_USER', CURRENT_TIMESTAMP(6), CURRENT_TIMESTAMP(6));
+
+            SET counter = counter + 10;  -- 10개의 레코드를 한 번에 추가
+        END WHILE;
+END$$
+DELIMITER ;
+
+CALL InsertBatchData();
+
+DROP PROCEDURE InsertBatchData;
